ee5669c871dd4318a95d9a576ec2e33274665df1
alina-sadykova <asadykovaa@gmail.com>
2025-05-13 20:18:55 -0500

    add reporters

---
+++ cypress/pages/TablesPage.js
@@ -0,0 +1,8 @@
+import BasePage from "./BasePage";
+
+class TablesPage extends BasePage {
+  getTableHeaders() {
+    return cy.get('th[class^="header"]');
+  }
+}
+export default TablesPage;
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,8 +17,12 @@
 import "./commands";
 import "@4tw/cypress-drag-drop";
 import "cypress-real-events";
+import "cypress-mochawesome-reporter/register";
 // import exec, { spawnSync } from 'child_process'
 // import spawn from 'child_process'
+
+const registerCypressGrep = require("@cypress/grep");
+registerCypressGrep();
 
 beforeEach(() => {
   cy.visit(`${Cypress.env("SITE_URL")}/frontend`);
@@ -27,7 +31,7 @@ beforeEach(() => {
 before(() => {
   // exec('node index.js')
   // spawn('node index.js')
-  //   cy.exec("node index.js");
+  cy.exec("node index.js");
   // cy.exec('react run')
 });
 
